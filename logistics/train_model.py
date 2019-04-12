7f5f0c9daeb13eb21aaf5726d095f43c0a4d1503
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-12 15:35:37 +0100

    First commit

---
+++ logistics/train_model.py
@@ -0,0 +1,14 @@
+#
+# Copyright (c) 2018-2019 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+#
+
+"""
+Tool Function used to train model
+"""
+
+# ------------------------
+#   IMPORTS
+# ------------------------
