ef4bdf5d99e1b306e2f9d7bdb692cff1df94040d
Pedro Silva <silva.mfpedro@gmail.com>
2019-04-12 16:10:50 +0100

    Added source files for logistic regression

--- logistics/evaluate_model.py
+++ logistics/evaluate_model.py
@@ -12,3 +12,21 @@ Tool Function used to evaluate model.
 # ------------------------
 #   IMPORTS
 # ------------------------
+
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+from sklearn.model_selection import cross_val_score
+
+# ------------------------
+#   Evaluate Function
+# ------------------------
+
+
+def evaluate(model, x_train, y_train):
+    predicted_data = model.predict(x_train)
+
+    # print evaluation stats
+    print("Score CV - test set:", cross_val_score(model, x_train, y_train, cv=10).mean())
+    print("Mean squared error - training set:", mean_squared_error(y_train, predicted_data))
+    print("Coefficient of determination - training set:", r2_score(y_train, predicted_data))
+    print("Accuracy - training set:", accuracy_score(y_train, predicted_data))
+    print("Cross_validation_evaluate: ", cross_val_score(model, x_train, y_train, cv=4))
--- logistics/train_model.py
+++ logistics/train_model.py
@@ -12,3 +12,15 @@ Tool Function used to train model
 # ------------------------
 #   IMPORTS
 # ------------------------
+
+from sklearn.linear_model import LogisticRegression
+
+# ----------------------------------
+# Logistic Regression Train Function
+# ----------------------------------
+
+
+def logistic_train(x_train, y_train, penalty, C, solver, multi_class, max_iter):
+    lg = LogisticRegression(penalty=penalty, C=C, solver=solver, multi_class=multi_class, max_iter=max_iter)
+    lg.fit(x_train, y_train)
+    return lg
