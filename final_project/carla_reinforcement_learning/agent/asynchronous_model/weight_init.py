0b40380a844c7b6c519a3ef1619db48c15bcab35
Pedro Silva <silva.mfpedro@gmail.com>
2019-06-07 14:56:52 +0100

    Added changes in v_function and weight_init modules

--- final_project/carla_reinforcement_learning/agent/asynchronous_model/weight_init.py
+++ final_project/carla_reinforcement_learning/agent/asynchronous_model/weight_init.py
@@ -8,3 +8,67 @@
 # ------------------------
 #   IMPORTS
 # ------------------------
+import numpy as np
+import chainer
+from chainer import links
+
+
+# ---------------------------
+#   Init with String Function
+# ---------------------------
+def init_with_str(link, init_str=""):
+    if init_str == "xavier":
+        xavier(link)
+    elif init_str.startswith("msra"):
+        # string should be in format "msra_0.2" , where the number is the negative slope
+        relu_neg_slope = float(init_str.split('_')[1])
+        msra(link, relu_neg_slope=relu_neg_slope)
+    else:
+        raise Exception('Unknown initialization method', init_str)
+
+
+# ---------------------------
+#   Xavier Function
+# ---------------------------
+def xavier(link):
+    # Mimic torch's default parameter initialization
+    # TODO(muupan): Use chainer's initializers when it is merged
+    for l in link.links():
+        if isinstance(l, links.Linear):
+            out_channels, in_channels = l.W.data.shape
+            stdv = 1 / np.sqrt(in_channels)
+            l.W.data[:] = np.random.uniform(-stdv, stdv, size=l.W.data.shape)
+            if l.b is not None:
+                l.b.data[:] = np.random.uniform(-stdv, stdv,
+                                                size=l.b.data.shape)
+        elif isinstance(l, links.Convolution2D):
+            out_channels, in_channels, kh, kw = l.W.data.shape
+            stdv = 1 / np.sqrt(in_channels * kh * kw)
+            l.W.data[:] = np.random.uniform(-stdv, stdv, size=l.W.data.shape)
+            if l.b is not None:
+                l.b.data[:] = np.random.uniform(-stdv, stdv,
+                                                size=l.b.data.shape)
+
+
+# ---------------------------
+#   MSRA Function
+# ---------------------------
+def msra(link, relu_neg_slope=0):
+    # Mimic torch's default parameter initialization
+    # TODO(muupan): Use chainer's initializers when it is merged
+    for l in link.links():
+        if isinstance(l, links.Linear):
+            out_channels, in_channels = l.W.data.shape
+            in_dim = in_channels
+        elif isinstance(l, links.Convolution2D):
+            out_channels, in_channels, kh, kw = l.W.data.shape
+            in_dim = in_channels * kh * kw
+        else:
+            return
+
+        stdv = 2 / np.sqrt(in_dim * (1 + relu_neg_slope ** 2))
+
+        l.W.data[:] = np.random.uniform(-stdv, stdv, size=l.W.data.shape)
+        if l.b is not None:
+            l.b.data[:] = np.random.uniform(-stdv, stdv,
+                                            size=l.b.data.shape)
