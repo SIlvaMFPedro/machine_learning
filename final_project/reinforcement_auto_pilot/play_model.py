a27524f288a7f1c3b08c73ef1addf3a22cfe8982
Pedro Silva <silva.mfpedro@gmail.com>
2019-06-11 22:17:53 +0100

    Added changes to reinforcement_auto_pilot module

---
+++ final_project/reinforcement_auto_pilot/play_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+#
+# Copyright (c) 2018-2019 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+#
+
+"""
+Once the model is learned, use this module to play it.
+"""
+
+# ------------------------
+#   IMPORTS
+# ------------------------
+import numpy as np
+import car_module
+from neural_network import neural_network
+
+NUM_SENSORS = 3
+
+
+# ------------------------
+#   Play Model Function
+# ------------------------
+def play(model):
+    car_distance = 0
+    game_state = car_module.GameState()
+
+    # Get initial state
+    _, state = game_state.frame_step(2)
+
+    # Take action move
+    while True:
+        car_distance += 1
+        # pick an action
+        action = (np.argmax(model.predict(state, batch_size=1)))
+        # take the action
+        _, state = game_state.frame_step(action)
+
+        # print values according to the action
+        if car_distance % 1000 == 0:
+            print("Current distance: %d frames." % car_distance)
+
+
+# ------------------------
+#   Main Function
+# ------------------------
+if __name__ == "__main__":
+    saved_model = 'saved-models/128-128-64-50000-50000.h5'
+    model = neural_network(NUM_SENSORS, [128, 128], saved_model)
+    play(model)
---
+++ final_project/reinforcement_auto_pilot/plot_model.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+#
+# Copyright (c) 2018-2019 Intel Corporation
+#
+# This work is licensed under the terms of the MIT license.
+# For a copy, see <https://opensource.org/licenses/MIT>.
+#
+
+"""
+This module takes the data in the results folder and plots the data
+"""
+
+# ------------------------
+#   IMPORTS
+# ------------------------
+import os
+import glob
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# --------------------------
+#   Moving Average Function
+# --------------------------
+def moving_average(y, window_size):
+    window = np.ones(int(window_size))/float(window_size)
+    return np.convolve(y, window, 'same')
+
+
+# --------------------------
+#   Readable Output Function
+# --------------------------
+def readable_output(filename):
+    readable = ''
+    f_parts = filename.split('-')
+
+    if f_parts[0] == 'learn_data':
+        readable += 'distance: '
+    else:
+        readable += 'loss: '
+
+    readable += f_parts[1] + ', ' + f_parts[2] + ' | '
+    readable += f_parts[3] + ' | '
+    readable += f_parts[4].split('.')[0]
+    return readable
+
+
+# --------------------------
+#   Plot Function
+# --------------------------
+def plot_file(filename, type='loss'):
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        # Turn the column into an array.
+        y = []
+        for row in reader:
+            if type == 'loss':
+                y.append(float(row[0]))
+            else:
+                y.append(float(row[1]))
+        # Running tests should be empty.
+        if len(y) == 0:
+            return
+        print(readable_output(filename))
+
+        # Get the moving average so the graph isn't so crazy.
+        if type == 'loss':
+            window = 100
+        else:
+            window = 10
+        y_av = moving_average(y, window)
+
+        # Use our moving average to get some metrics.
+        array = np.array(y_av)
+        if type == 'loss':
+            print("%f\t%f\n" % (array.min(), array.mean()))
+        else:
+            print("%f\t%f\n" % (array.max(), array.mean()))
+
+        # Plot it.
+        plt.clf()  # Clear.
+        plt.title(filename)
+        # The -50 removes an artificial drop at the end caused by the moving
+        # average.
+        if type == 'loss':
+            plt.plot(y_av[:-50])
+            plt.ylabel('Smoothed Loss')
+            plt.ylim(0, 5000)
+            plt.xlim(0, 250000)
+        else:
+            plt.plot(y_av[:-5])
+            plt.ylabel('Smoothed Distance')
+            plt.ylim(0, 4000)
+
+        plt.savefig(filename + '.png', bbox_inches='tight')
+
+
+# --------------------------
+#   Main Function
+# --------------------------
+if __name__ == "__main__":
+    # Get the result files.
+    os.chdir("results/frames")
+
+    for f in glob.glob("learn*.csv"):
+        plot_file(f, 'learn')
+
+    for f in glob.glob("loss*.csv"):
+        plot_file(f, 'loss')
